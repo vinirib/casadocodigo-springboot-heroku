f35ccff3d8910988aedb7f79325ec9a299b1b64a
vinicius <vinicius@gabi-Aspire-V5-471>
2017-04-16 08:25:39 -0300

    Implemented Session Logout

--- casadocodigo-springboot/src/main/java/br/com/casadocodigo/loja/config/SecurityConfiguration.java
+++ casadocodigo-springboot/src/main/java/br/com/casadocodigo/loja/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/produtos/form").hasRole("ADMIN")
 			.antMatchers("/carrinho/**").permitAll()
+			.antMatchers("/pagamento/**").permitAll()
 			.antMatchers(HttpMethod.GET, "/produtos").permitAll()
 			.antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
 			.antMatchers("/produtos/**").permitAll()
--- casadocodigo-springboot/src/main/java/br/com/casadocodigo/loja/web/PagamentoController.java
+++ casadocodigo-springboot/src/main/java/br/com/casadocodigo/loja/web/PagamentoController.java
@@ -2,8 +2,14 @@ package br.com.casadocodigo.loja.web;
 
 import java.util.concurrent.Callable;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,11 +37,15 @@ public class PagamentoController {
 	private static final String urlPagamento = "http://book-payment.herokuapp.com/payment"; 
 	
     @RequestMapping(value="/finalizar", method=RequestMethod.POST)
-    public Callable<ModelAndView> finalizar(@AuthenticationPrincipal User usuario, RedirectAttributes model){
+    public Callable<ModelAndView> finalizar(@AuthenticationPrincipal User usuario, RedirectAttributes model, HttpServletRequest request, HttpServletResponse response){
     	return () -> {
     		
 	    	try {
 	    		Pagamento pagamento = new Pagamento(carrinho.getTotal());
+	    		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    	    if (auth != null){    
+	    	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    	    }
 //	    		String response = restTemplate.postForObject(urlPagamento, pagamento, String.class);
 //	    		model.addFlashAttribute("sucesso", response);
 //	    		enviaEmailCompraProduto(usuario);
